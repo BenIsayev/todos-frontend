337f679edbb086981eadd29d2f327bacd0ec8f46
BenIsayev <benisayev@gmail.com>
2022-06-16 12:58:51 +0300

    minor editing

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,12 @@
+import { NavLink } from 'react-router-dom'
+
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <section>
+            <div>Hello! this is a todo app and we are happy to see you here!</div>
+            <img src="https://c.tenor.com/t6zKtQ38LhAAAAAC/it-is-so-great-to-have-you-here-good-for-you-to-be-here.gif" alt="" />
+            <NavLink to="/todo">Get started</NavLink>
+        </section>
     )
 }
 
